8dd451ee334dadf70ee52a04b57ae2bdda1a00b1
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-10-23 09:53:44 -0500

    Migrate to new stepper motors hardware

--- firmware/board.c
+++ firmware/board.c
@@ -2,100 +2,107 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define X_CYCLES_DEFAULT 50
-#define Y_CYCLES_DEFAULT 100
+#define X_MICRO_STEPS 50
+#define Y_MICRO_STEPS 50
+#define X_WAIT 12
+#define Y_WAIT 10
 
-static int X_CYCLES = X_CYCLES_DEFAULT;
-static int Y_CYCLES = Y_CYCLES_DEFAULT;
-
-void x_forward_cycle();
-void x_backward_cycle();
-void y_forward_cycle();
-void y_backward_cycle();
+void x_micro_fstep();
+void x_micro_bstep();
+void y_micro_fstep();
+void y_micro_bstep();
 void reset();
+
+int X_STATE = 0x01;
+int Y_STATE = 0x10;
 
 
 void board_init()
 {
     DDRC = 0b00001111;
     PORTC = 0x00;
+    DDRD = 0b01111000;
+    PORTD |= 0b00000000;
     reset();
 }
 
 void reset()
 {
     _delay_ms(1000);
+    // Reset X axis
     while (PINC & (1 << PINC5))
     {
-        x_backward_cycle();
+        x_micro_bstep();
     }
 
-    while (PINC & (1 << PINC4))
-    {
-        y_backward_cycle();
-    }
+    // Reset Y axis
+    /* y_bstep(); */
+    /* while (PINC & (1 << PINC4)) */
+    /* { */
+    /*     y_micro_bstep(); */
+    /* } */
 }
 
 void x_fstep()
 {
-    for (int i=0; i<X_CYCLES; i++)
+    for (int i=0; i<X_MICRO_STEPS; i++)
     {
-        x_forward_cycle();
+        x_micro_fstep();
     }
 }
 
 void x_bstep()
 {
-    for (int i=0; i<X_CYCLES; i++)
+    for (int i=0; i<X_MICRO_STEPS; i++)
     {
-        x_backward_cycle();
+        x_micro_bstep();
     }
 }
 
 void y_fstep()
 {
-    for (int i=0; i<Y_CYCLES; i++)
+    for (int i=0; i<Y_MICRO_STEPS; i++)
     {
-        y_forward_cycle();
+        y_micro_fstep();
     }
 }
 
 void y_bstep()
 {
-    for (int i=0; i<Y_CYCLES; i++)
+    for (int i=0; i<Y_MICRO_STEPS; i++)
     {
-        x_backward_cycle();
+        y_micro_bstep();
     }
 }
 
-void x_forward_cycle()
+void x_micro_fstep()
 {
-    PORTC |= (1 << PORTC0);
-    _delay_us(1500);
-    PORTC &= (0 << PORTC0);
-    _delay_ms(10);
+    PORTC = X_STATE;
+    _delay_ms(X_WAIT);
+    PORTC = 0x00;
+    X_STATE = (X_STATE == 0x08)? 0x01 : (X_STATE <<= 1);
 }
 
-void x_backward_cycle()
+void x_micro_bstep()
 {
-    PORTC |= (1 << PORTC1);
-    _delay_us(1500);
-    PORTC &= (0 << PORTC1);
-    _delay_ms(10);
+    PORTC = X_STATE;
+    _delay_ms(X_WAIT);
+    PORTC = 0x00;
+    X_STATE = (X_STATE == 0x01)? 0x08 : (X_STATE >>= 1);
 }
 
-void y_forward_cycle()
+void y_micro_fstep()
 {
-    PORTC |= (1 << PORTC2);
-    _delay_us(4000);
-    PORTC &= (0 << PORTC2);
-    _delay_ms(10);
+    PORTD = Y_STATE;
+    _delay_ms(Y_WAIT);
+    PORTD = 0x00;
+    Y_STATE = (Y_STATE == 0x80)? 0x10 : (Y_STATE <<= 1);
 }
 
-void y_backward_cycle()
+void y_micro_bstep()
 {
-    PORTC |= (1 << PORTC3);
-    _delay_us(4000);
-    PORTC &= (0 << PORTC3);
-    _delay_ms(10);
+    PORTD = Y_STATE;
+    _delay_ms(Y_WAIT);
+    PORTD = 0x00;
+    Y_STATE = (Y_STATE == 0x10)? 0x80 : (Y_STATE >>= 1);
 }
--- firmware/board.h
+++ firmware/board.h
@@ -2,6 +2,7 @@
 #define _BOARD_H
 
 void board_init();
+void board_calibration();
 void x_fstep();
 void y_fstep();
 void x_bstep();
