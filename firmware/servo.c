c66c76472ea3d686f1a88fab06c10ad9cfba14ce
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-10-30 21:14:00 -0500

    Firmware: Fix servo.c style

--- firmware/servo.c
+++ firmware/servo.c
@@ -13,12 +13,13 @@
 
 void servo_init()
 {
-    TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11); // NON Inverted PWM
-    TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); // Fast PWM
-    ICR1=4999; // PWM = 50Hz
-    DDRB|=(1<<PORTB1)|(1<<PORTB2);
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11); // NON Inverted PWM
+    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11) | (1 << CS10);
+    ICR1 = 4999; // PWM = 50Hz
+    DDRB |= (1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2);
     servo_reset();
 }
+
 
 void servo_reset()
 {
@@ -47,18 +48,18 @@ void servo_set(angle a)
 
 void servo_fstep()
 {
-        OCR1A += SERVO_STEP;
-        if (OCR1A >= SERVO_ANGLE_END)
-        {
-            servo_set(ANGLE_END);
-        }
+    OCR1A += SERVO_STEP;
+    if (OCR1A >= SERVO_ANGLE_END)
+    {
+        servo_set(ANGLE_END);
+    }
 }
 
 void servo_bstep()
 {
-        OCR1A -= SERVO_STEP;
-        if (OCR1A <= SERVO_ANGLE_START)
-        {
-            servo_set(ANGLE_START);
-        }
+    OCR1A -= SERVO_STEP;
+    if (OCR1A <= SERVO_ANGLE_START)
+    {
+        servo_set(ANGLE_START);
+    }
 }
