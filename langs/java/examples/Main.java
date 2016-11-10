b72151a7e64cab742f905e7e40d1a4edad7c87c8
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-11-09 19:24:21 -0500

    Langs: Add first Java lib

---
+++ langs/java/examples/Main.java
@@ -0,0 +1,9 @@
+import codebot.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Codebot.step();
+    }
+}
---
+++ langs/java/lib/src/codebot/Codebot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codebot;
+
+/**
+ *
+ * @author alx
+ */
+public class Codebot {
+    public static void step()
+    {
+        System.out.println("step");
+    }
+    
+}
