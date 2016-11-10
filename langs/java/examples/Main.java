211f40c11f1926a11911de6b5a26329b67fddb3d
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-11-09 20:07:07 -0500

    Langs: First Java version

--- langs/java/examples/Main.java
+++ langs/java/examples/Main.java
@@ -1,9 +1,36 @@
-import codebot.*;
+import codebot.Codebot;
+import codebot.Codebot.Axis;
+import codebot.Codebot.Direction;
 
 public class Main
 {
     public static void main(String args[])
     {
-        Codebot.step();
+        System.out.println("\n-- Stepping");
+        Codebot.step(Axis.X, Direction.FORWARD);
+        Codebot.step(Axis.Y, Direction.FORWARD);
+        Codebot.step(Axis.X, Direction.BACKWARD);
+        Codebot.step(Axis.Y, Direction.BACKWARD);
+
+        System.out.println("\n-- Jumping");
+        Codebot.jump(Axis.X, Direction.FORWARD);
+        Codebot.jump(Axis.Y, Direction.FORWARD);
+        Codebot.jump(Axis.X, Direction.BACKWARD);
+        Codebot.jump(Axis.Y, Direction.BACKWARD);
+
+        System.out.println("\n-- Opening Doors");
+        Codebot.openDoor(Axis.X, Direction.FORWARD);
+        Codebot.openDoor(Axis.Y, Direction.FORWARD);
+        Codebot.openDoor(Axis.X, Direction.BACKWARD);
+        Codebot.openDoor(Axis.Y, Direction.BACKWARD);
+
+        System.out.println("\n-- Picking up objects");
+        Codebot.pickUp();
+
+        System.out.println("\n-- Dropping objects");
+        Codebot.drop();
+
+        System.out.println("\n-- Interacting with elements");
+        Codebot.interact();
     }
 }
--- langs/java/lib/src/codebot/Codebot.java
+++ langs/java/lib/src/codebot/Codebot.java
@@ -1,18 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codebot;
 
-/**
- *
- * @author alx
- */
-public class Codebot {
-    public static void step()
+public class Codebot
+{
+    public static enum Axis { X, Y }
+    public static enum Direction { FORWARD, BACKWARD }
+
+    public static void step(Axis axis, Direction direction)
     {
-        System.out.println("step");
+        movement("Step", axis, direction);
     }
-    
+
+    public static void jump(Axis axis, Direction direction)
+    {
+        movement("Jump", axis, direction);
+    }
+
+    public static void openDoor(Axis axis, Direction direction)
+    {
+        movement("Open Door", axis, direction);
+    }
+
+    public static void pickUp()
+    {
+        System.out.println("Pick Up");
+    }
+
+    public static void drop()
+    {
+        System.out.println("Drop");
+    }
+
+    public static void interact()
+    {
+        System.out.println("Interact");
+    }
+
+    private static void movement(String string, Axis axis, Direction direction)
+    {
+        if (axis == Axis.X)
+        {
+            switch (direction)
+            {
+                case FORWARD:
+                    System.out.println(string + " Forward X");
+                    break;
+
+                case BACKWARD:
+                    System.out.println(string + " Backward X");
+                    break;
+            }
+        }
+        else if (axis == Axis.Y)
+        {
+            switch (direction)
+            {
+                case FORWARD:
+                    System.out.println(string + " Forward Y");
+                    break;
+
+                case BACKWARD:
+                    System.out.println(string + " Backward Y");
+                    break;
+            }
+        }
+    }
 }
