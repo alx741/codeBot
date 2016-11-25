91256c2f320da1157a4c28ed4a7a431c3647abac
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-11-25 18:37:14 -0500

    Add isCommand predicate tests

--- src/codebot/test/test.hs
+++ src/codebot/test/test.hs
@@ -3,5 +3,19 @@ import Test.Tasty.HUnit
 
 import Data.List
 import Data.Ord
+import Execution
+import Command
 
-main = putStrLn "TODO"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Execution" [commands]
+
+commands =
+    testGroup
+        "Execution"
+        [ testCase "Jump Forward X Is Command" $
+          assertBool "Is a command" (isCommand "Jump Forward X")
+        , testCase "OpenDoor Backward Y Is Command" $
+          assertBool "Is a command" (isCommand "OpenDoor Backward Y")
+        ]
