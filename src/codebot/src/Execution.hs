4d8279c00607a17de044ebadd6c1e0e570fb1927
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-11-25 17:37:33 -0500

    First Execution module

---
+++ src/codebot/src/Execution.hs
@@ -0,0 +1,18 @@
+module Execution where
+
+import Command
+import Data.List
+import Safe
+
+validCommands = ["Step", "Jump", "OpenDoor", "PickUp", "Drop", "Interact"]
+
+readCommandsFile :: FilePath -> IO [Command]
+readCommandsFile fp = readFile fp >>= return . map read . filter isCommand . lines
+
+isCommand :: String -> Bool
+isCommand str =
+    case firstWord of
+        Just word -> word `elem` validCommands
+        Nothing -> False
+  where
+    firstWord = headMay . words $ str
