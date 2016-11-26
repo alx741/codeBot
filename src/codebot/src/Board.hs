8cb7993b278daa5351cfb7700fac0b731796763a
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-11-26 16:51:47 -0500

    Polish monadic movement

--- src/codebot/src/Board.hs
+++ src/codebot/src/Board.hs
@@ -1,14 +1,17 @@
 module Board where
 
+import Command
+
 type Board = [[Cell]]
 
 data Position = Position
     { x :: Int
     , y :: Int
+    , z :: Int
     } deriving (Show, Eq)
 
 data Cell
-    = Tile
+    = Tile Int
     | Empty
     | Door DoorState
     | Obstacle
@@ -39,14 +42,74 @@ data ObjectType
 readBoardFile :: FilePath -> Board
 readBoardFile = undefined
 
+getCell :: Position -> Board -> Maybe Cell
+getCell position board =
+    case isValidPosition position of
+        Just position' -> Just (board !! x position' !! y position')
+        Nothing -> Nothing
+
+move :: Position -> Direction -> Axis -> Maybe Position
+move pos Forward axis = applyMovement (+ 1) axis pos
+move pos Backward axis = applyMovement (\x -> x - 1) axis pos
+
+applyMovement :: (Int -> Int) -> Axis -> Position -> Maybe Position
+applyMovement f X position = isValidPosition position'
+  where
+    position' =
+        Position
+        { x = f $ x position
+        , y = y position
+        , z = z position
+        }
+applyMovement f Y position = isValidPosition position'
+  where
+    position' =
+        Position
+        { x = x position
+        , y = f $ y position
+        , z = z position
+        }
+
+isValidPosition :: Position -> Maybe Position
+isValidPosition position
+    | validX && validY = Just position
+    | otherwise = Nothing
+  where
+    validX = x position >= 0 && x position <= 7
+    validY = y position >= 0 && y position <= 7
+
 board :: Board
 board =
-    [ [Tile, Tile, Tile, Tile, Tile, Object Triangle, Tile, Tile]
-    , [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile]
-    , [Tile, Tile, Object Circle, Tile, Tile, Tile, Tile, Tile]
-    , [Tile, Tile, Tile, Tile, Tile, Tile, Tile, Tile]
-    , [Tile, Tile, Tile, Tile, Tile, Tile, Socket Triangle SocketEmpty, Tile]
-    , [Tile, Tile, Socket Circle SocketEmpty, Tile, Tile, Tile, Tile, Tile]
-    , [Tile, Tile, Tile, Tile, Door Close, Tile, Tile, Tile]
-    , [Tile, Tile, Object Key, Tile, Tile, Tile, Tile, Tile]
+    [ [ Tile 1
+      , Tile 0
+      , Tile 1
+      , Tile 2
+      , Tile 0
+      , Object Triangle
+      , Tile 0
+      , Tile 0
+      ]
+    , [Door Close, Empty, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0]
+    , [Tile 0, Tile 0, Object Circle, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0]
+    , [Tile 0, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0]
+    , [ Tile 0
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      , Socket Triangle SocketEmpty
+      , Tile 0
+      ]
+    , [ Tile 0
+      , Tile 0
+      , Socket Circle SocketEmpty
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      , Tile 0
+      ]
+    , [Tile 0, Tile 0, Tile 0, Tile 0, Door Close, Tile 0, Tile 0, Tile 0]
+    , [Tile 0, Tile 0, Object Key, Tile 0, Tile 0, Tile 0, Tile 0, Tile 0]
     ]
