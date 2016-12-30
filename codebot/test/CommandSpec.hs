ba532d312884b6feb150a5c62b9537631328b119
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-12-30 11:40:43 -0500

    Test Command module

--- codebot/test/CommandSpec.hs
+++ codebot/test/CommandSpec.hs
@@ -10,3 +10,8 @@ spec = do
     describe "isCommand" $ do
         it "returns True when the argument is a valid command" $ do
             (isCommand "Jump Forward X") `shouldBe` True
+            (isCommand "Step Backward X") `shouldBe` True
+            (isCommand "OpenDoor Backward Y") `shouldBe` True
+            (isCommand "PickUp") `shouldBe` True
+            (isCommand "Drop") `shouldBe` True
+            (isCommand "Interact") `shouldBe` True
